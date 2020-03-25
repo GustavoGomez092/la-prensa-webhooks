d15fd4fc119d14dace1326b3f16933c4bd8c710c
gustavogomez092 <gustavogomez092@gmail.com>
2020-03-25 02:15:27 -0600

    Received data from webhook and fetched convertion data from API

--- index.js
+++ index.js
@@ -1,20 +1,80 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var app = express()
+const express = require('express')
+const bodyParser = require('body-parser')
+const axios = require('axios').default
+const env = require('dotenv').config().parsed
+const app = express()
+const crypto = require('crypto')
 
 const PORT = process.env.PORT || 3000
+
+const { API_TOKEN, PRIVATE_KEY } = env
+
+// Data decoder
+const keystring = PRIVATE_KEY
+
+function urldesafe (data) {
+  const newData = data.replace('-_', '+/')
+  return newData
+}
+
+function sslDecrypt (keystring, str) {
+  var mykey = crypto.createDecipheriv('aes-256-ecb', keystring, '')
+  mykey.setAutoPadding(false)
+  var mystr = mykey.update(str, 'base64', 'utf8')
+  mystr += mykey.final('utf8')
+  return mystr
+}
+
+function decrypt (keyString, data) {
+  const pos = data.lastIndexOf('~~~')
+  if (pos > 0) {
+    data = data.substr(0, pos)
+  }
+
+  data = urldesafe(data)
+  if (keyString.length > 32) {
+    keyString = keyString.substr(0, 32)
+  }
+
+  if (keyString.length < 32) {
+    keyString = keyString.padStart(32, 'X')
+  }
+  function encodeUtf8 (s) {
+    return s.replace(/("}).*/g, '"}')
+  }
+
+  const mystring = encodeUtf8(sslDecrypt(keyString, data))
+  return mystring
+}
 
 // middleware
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.post('/', function (req, res) {
-  console.log(req.body)
-  res.end('OK 200')
-})
-
 app.get('/', function (req, res) {
-  console.log(req.body)
+  const responseOpject = JSON.parse(decrypt(keystring, req.query.data))
+  console.log(responseOpject)
+
+  // Make a request for a user with a given ID
+  axios.get('https://sandbox.tinypass.com/api/v3/publisher/conversion/get', {
+    params: {
+      api_token: API_TOKEN,
+      aid: responseOpject.aid,
+      access_id: responseOpject.access_id
+    }
+  })
+    .then(function (response) {
+      // handle success
+      console.log(response.data)
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error)
+    })
+    .then(function () {
+      res.end('OK 200')
+    })
+
   res.end('OK 200')
 })
 
